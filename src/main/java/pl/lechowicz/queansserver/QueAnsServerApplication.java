dd75fc083cda88e83fa056933d65e66107fda21c
Michael Lechowicz <ichal.lechowicz@gmail.com>
2023-12-02 18:02:32 +0100

    Remove unused import

--- src/main/java/pl/lechowicz/queansserver/QueAnsServerApplication.java
+++ src/main/java/pl/lechowicz/queansserver/QueAnsServerApplication.java
@@ -2,14 +2,6 @@ package pl.lechowicz.queansserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-import pl.lechowicz.queansserver.entry.entity.AnswerEntity;
-import pl.lechowicz.queansserver.entry.entity.EntryEntity;
-import pl.lechowicz.queansserver.entry.entity.QuestionEntity;
-
-import java.util.Set;
-
 @SpringBootApplication
 public class QueAnsServerApplication {
 
